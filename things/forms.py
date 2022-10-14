f60b09f00bbc300d1190baec7968c60f41b54139
Archie Sage <archie.sage@kcl.ac.uk>
2022-10-14 14:40:11 +0100

    Implement ThingForm on home page

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
-"""Forms of the project."""
+from django import forms
+from things.models import Thing
+from django.core.validators import RegexValidator
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity' ]
+        widgets =  { 
+            'description': forms.Textarea(),
+            'quantity' : forms.NumberInput(),
+            }
